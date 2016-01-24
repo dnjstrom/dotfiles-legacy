8d1cccd94310f5bc0d6a74fa49ce8d7e3fffaeef
Daniel Ström <D@nielstrom.se>
2016-01-24 14:25:29 +0100

    Add zsh configuration using oh-my-zsh

---
+++ zsh/aliases.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+alias zshrc="vim ~/.zshrc"
+alias reload="source ~/.zshrc"
---
+++ zsh/provision.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# git submodule init ~/.dotfiles/zsh/.oh-my-zsh
+
+rm -fr ~/.oh-my-zsh
+ln -sfFvh ~/.dotfiles/zsh/oh-my-zsh ~/.oh-my-zsh
+
+cat ~/.dotfiles/zsh/oh-my-zsh/templates/zshrc.zsh-template |\
+	sed -e 's/ZSH_THEME="robbyrussell"/ZSH_THEME="bureau"/' |\
+	sed -e 's/plugins=(git)/plugins=(autojump docker docker-compose git history-substring-search npm nvm node themes)/' \
+	> ~/.dotfiles/zsh/zshrc
+
+ln -sfFvh ~/.dotfiles/zsh/zshrc	~/.zshrc
+
+# Zsh aliases
+line="source ~/.dotfiles/zsh/aliases.sh"
+
+if grep -q "$line" ~/.zshrc; then
+	echo "zsh aliases already present in .zshrc."
+else
+	echo "Adding zsh aliases to .zshrc."
+	echo "" >> ~/.zshrc
+	echo "# Define some utility aliases for zsh" >> ~/.zshrc
+	echo "$line" >> ~/.zshrc
+fi
