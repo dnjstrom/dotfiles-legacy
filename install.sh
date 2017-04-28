70b7d47f4807123453f88f7541f6f15465546f16
Daniel Ström <D@nielstrom.se>
2017-04-28 14:34:50 +0200

    Quick installation script for utilities

---
+++ install.sh
@@ -0,0 +1,6 @@
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install ctop nvm tmux rbenv reattach-to-user-namespace fish fisherman neovim/neovim/neovim ctags
+
+npm i -g yarn tern
