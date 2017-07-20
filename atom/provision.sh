03d46f834009800a6c61670af494dd2ea740fffb
Daniel Ström <D@nielstrom.se>
2017-07-20 09:18:40 +0200

    Store atom config

---
+++ atom/provision.sh
@@ -0,0 +1,1 @@
+apm install nucleus-dark-ui gruvbox-plus-syntax atom-clock atom-ternjs editorconfig emmet linter linter-eslint minimap-linter minimap vim-mode-plus vim-mode-plus-ex-mode vim-mode-plus-keymaps-for-surround
