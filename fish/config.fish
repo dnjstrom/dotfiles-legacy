02afbd5fd52638196b309e3a66285285adced4cb
Daniel Ström <daniel.strom@eqtpartners.com>
2020-05-12 12:33:40 +0200

    Save

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,7 @@
 # Environment variables
 # Set english locale
-set -x  LC_ALL en_US.UTF-8  
-set -x LANG en_US.UTF-8  
+set -x LC_ALL en_US.UTF-8
+set -x LANG en_US.UTF-8
 set -x EDITOR nvim
 set -x PATH $PATH /Users/daniel/.local/bin
 set -x PATH $PATH /Users/daniel/.cargo/bin
@@ -22,42 +22,112 @@ set fish_greeting
 # end
 
 if not set -q abbrs_initialized
-  set -U abbrs_initialized
-  
-  abbr vim 'nvim'
-  abbr d 'docker'
-  abbr dc 'docker-compose'
-  abbr ga 'git add'
-  abbr gA 'git add -A :/'
-  abbr gc 'git checkout'
-  abbr gC 'git commit'
-  abbr gAC 'git add -A :/; git commit'
-  abbr gf 'git fetch'
-  abbr gp 'git pull --rebase'
-  abbr gP 'git push'
-  abbr gr 'git rebase'
-  abbr gs 'git status'
-  abbr gd 'git -c color.ui=always status -v -v | less -RX'
-  abbr gl 'git log --oneline --graph'
-  abbr gb 'git branch'
-  abbr geach 'git submodule foreach'
-  abbr grekt 'git reset --hard HEAD'
-  abbr cd- 'cd -'
-  abbr y 'yarn'
-  abbr ys 'yarn start'
-  abbr yd 'yarn dev'
-  abbr yb 'yarn build'
-  abbr l 'la'
-  abbr c 'code .'
-  abbr gitclean 'git checkout master; and git fetch -p; and git pull; and git branch --merged | egrep -v "(^\*|master)" | xargs git branch -d; and git fetch --prune'
+    set -U abbrs_initialized
+
+    abbr vim 'nvim'
+    abbr d 'docker'
+    abbr dc 'docker-compose'
+    abbr ga 'git add'
+    abbr gA 'git add -A :/'
+    abbr gc 'git checkout'
+    abbr gC 'git commit'
+    abbr gAC 'git add -A :/; git commit'
+    abbr gf 'git fetch'
+    abbr gp 'git pull --rebase'
+    abbr gP 'git push'
+    abbr gr 'git rebase'
+    abbr gs 'git status'
+    abbr gd 'git -c color.ui=always status -v -v | less -RX'
+    abbr gl 'git log --oneline --graph'
+    abbr gb 'git branch'
+    abbr geach 'git submodule foreach'
+    abbr grekt 'git reset --hard HEAD'
+    abbr cd- 'cd -'
+    abbr y 'yarn'
+    abbr ys 'yarn start'
+    abbr yd 'yarn dev'
+    abbr yb 'yarn build'
+    abbr l 'la'
+    abbr c 'code .'
+    abbr gitclean 'git checkout master; and git fetch -p; and git pull; and git branch --merged | egrep -v "(^\*|master)" | xargs git branch -d; and git fetch --prune'
 end
 
 function tmux_func
-  if tmux ls | grep -vq attached
-    tmux attach
-  else
-    tmux
-  end
+    if tmux ls | grep -vq attached
+        tmux attach
+    else
+        tmux
+    end
+end
+
+function read_confirm
+    while true
+        read -l -P 'Do you want to continue? [y/N] ' confirm
+
+        switch $confirm
+            case Y y
+                return 0
+            case '' N n
+                return 1
+        end
+    end
+end
+
+function push-to-staging
+    git fetch
+    echo -e "The latest commit to eye-staging was made" (git show --format="%cr" origin/staging | head -n 1)
+    echo ""
+    echo (git show --format="\"%s\" by %an" origin/staging | head -n 1)
+    echo ""
+
+    if read_confirm
+        git push -f origin (git rev-parse --abbrev-ref HEAD):staging
+    else
+        echo "Quitting without pushing"
+    end
+end
+
+
+function push-to-eylin
+    git fetch
+    echo -e "The latest commit to eylin was made" (git show --format="%cr" origin/eylin | head -n 1)
+    echo ""
+    echo (git show --format="\"%s\" by %an" origin/eylin | head -n 1)
+    echo ""
+
+    if read_confirm
+        git push -f origin (git rev-parse --abbrev-ref HEAD):eylin
+    else
+        echo "Quitting without pushing"
+    end
+end
+
+function push-to-eymon
+    git fetch
+    echo -e "The latest commit to eymon was made" (git show --format="%cr" origin/eymon | head -n 1)
+    echo ""
+    echo (git show --format="\"%s\" by %an" origin/eymon | head -n 1)
+    echo ""
+
+    if read_confirm
+        git push -f origin (git rev-parse --abbrev-ref HEAD):eymon
+    else
+        echo "Quitting without pushing"
+    end
+end
+
+function push-to-eyoar
+    git fetch
+    echo -e "The latest commit to eyoar was made" (git show --format="%cr" origin/eyoar | head -n 1)
+    echo ""
+    echo (git show --format="\"%s\" by %an" origin/eyoar | head -n 1)
+    echo ""
+
+    if read_confirm
+        git push -f origin (git rev-parse --abbrev-ref HEAD):eyoar
+    else
+        echo "Quitting without pushing"
+    end
 end
 
 starship init fish | source
--- phoenix.js
+++ phoenix.js
@@ -16,13 +16,13 @@
 
 const gutter = 14;
 
-const tallMonitor = 1;
-const middleMonitor = 2;
+const tallMonitor = 2;
+const middleMonitor = 1;
 const offMonitor = 0;
 
 /* Helpers */
 
-const simpleRule = rule => () => {
+const simpleRule = (rule) => () => {
   const window = Window.focused();
   const visible = window.screen().visibleFrame();
   const full = window.screen().flippedVisibleFrame();
@@ -36,50 +36,50 @@ const simpleRule = rule => () => {
     x,
     y,
     width: visible.width,
-    height: visible.height
+    height: visible.height,
   });
 };
 
-const oneThird = x => x / 3;
-const twoThirds = x => (2 * x) / 3;
+const oneThird = (x) => x / 3;
+const twoThirds = (x) => (2 * x) / 3;
 
 /* Operations */
 
-const left = window => {
+const left = (window) => {
   const screen = window.screen().flippedVisibleFrame();
   window.setFrame({
     x: screen.x + gutter,
     y: screen.y + gutter,
     width: Math.round(twoThirds(screen.width) - 1.5 * gutter),
-    height: screen.height - 2 * gutter
+    height: screen.height - 2 * gutter,
   });
 };
 
-const right = window => {
+const right = (window) => {
   const screen = window.screen().flippedVisibleFrame();
   window.setFrame({
     x: Math.round(screen.x + twoThirds(screen.width) + 0.5 * gutter),
     y: screen.y + gutter,
     width: Math.round(oneThird(screen.width) - 1.5 * gutter),
-    height: screen.height - 2 * gutter
+    height: screen.height - 2 * gutter,
   });
 };
 
-const up = window => {
+const up = (window) => {
   const screen = window.screen().flippedVisibleFrame();
   window.setFrame({
     ...window.frame(),
     y: screen.y + gutter,
-    height: Math.round(screen.height / 2 - 1.5 * gutter)
+    height: Math.round(screen.height / 2 - 1.5 * gutter),
   });
 };
 
-const down = window => {
+const down = (window) => {
   const screen = window.screen().flippedVisibleFrame();
   window.setFrame({
     ...window.frame(),
     y: Math.round(screen.y + screen.height / 2 + 0.5 * gutter),
-    height: Math.round(screen.height / 2 - 1.5 * gutter)
+    height: Math.round(screen.height / 2 - 1.5 * gutter),
   });
 };
 
@@ -88,7 +88,7 @@ const topleft = simpleRule((window, screen) => {
     x: screen.x,
     y: screen.y,
     width: 2 * (screen.width / 3),
-    height: screen.height / 2
+    height: screen.height / 2,
   });
 });
 
@@ -97,7 +97,7 @@ const topright = simpleRule((window, screen) => {
     x: screen.x + 2 * (screen.width / 3),
     y: screen.y,
     width: screen.width / 3,
-    height: screen.height / 2
+    height: screen.height / 2,
   });
 });
 
@@ -106,7 +106,7 @@ const bottomleft = simpleRule((window, screen) => {
     x: screen.x,
     y: screen.y + screen.height / 2,
     width: 2 * (screen.width / 3),
-    height: screen.height / 2
+    height: screen.height / 2,
   });
 });
 
@@ -115,26 +115,23 @@ const bottomright = simpleRule((window, screen) => {
     x: screen.x + 2 * (screen.width / 3),
     y: screen.y + screen.height / 2,
     width: screen.width / 3,
-    height: screen.height / 2
+    height: screen.height / 2,
   });
 });
 
-const fullsize = window => {
+const fullsize = (window) => {
   const screen = window.screen().flippedVisibleFrame();
   window.setFrame({
     x: screen.x + gutter,
     y: screen.y + gutter,
     width: screen.width - 2 * gutter,
-    height: screen.height - 2 * gutter
+    height: screen.height - 2 * gutter,
   });
 };
 
 const fullsizeFocusedWindow = () => fullsize(Window.focused());
 
-const fullsizeAllWindows = () =>
-  Screen.main()
-    .windows()
-    .map(fullsize);
+const fullsizeAllWindows = () => Screen.main().windows().map(fullsize);
 
 const fullscreen = () => {
   const window = Window.focused();
@@ -144,126 +141,129 @@ const fullscreen = () => {
   window.setFullScreen(!window.isFullScreen());
 };
 
-
-const devLayout = [ 
+const devLayout = [
   {
-  "": window => {
-    // Screen 0 is always the primary window
-    moveToScreen(0)(window);
-  }
+    "": (window) => {
+      // Screen 0 is always the primary window
+      moveToScreen(0)(window);
+    },
   },
   {
-    "Google Chrome": window => {
+    "Google Chrome": (window) => {
       moveToScreen(tallMonitor)(window);
     },
-    Code: window => {
+    Code: (window) => {
       moveToScreen(tallMonitor)(window);
       left(window);
     },
-    Hyper: window => {
+    Hyper: (window) => {
       moveToScreen(tallMonitor)(window);
       right(window);
     },
-    "": window => {
+    "": (window) => {
       moveToScreen(offMonitor)(window);
-    }
-  } , 
-  {
-    "Google Chrome": window => {
-      moveToScreen(tallMonitor)(window);
     },
-    Code: window => {
-      moveToScreen(middleMonitor)(window);
-      left(window);
-    },
-    Hyper: window => {
-      moveToScreen(middleMonitor)(window);
-      right(window);
-    },
-    "": window => {
-      moveToScreen(offMonitor)(window);
-    }
-  }];
-
-const adminLayout = [ 
-  {
-  "": window => {
-    // Screen 0 is always the primary window
-    moveToScreen(0)(window);
-  }
   },
   {
-    "Google Chrome": window => {
-      moveToScreen(middleMonitor)(window);
+    "Google Chrome": (window) => {
+      moveToScreen(tallMonitor)(window);
     },
-    Code: window => {
+    Code: (window) => {
       moveToScreen(middleMonitor)(window);
       left(window);
     },
-    Hyper: window => {
+    Hyper: (window) => {
       moveToScreen(middleMonitor)(window);
       right(window);
     },
-    "": window => {
+    "": (window) => {
       moveToScreen(offMonitor)(window);
-    }
-  }, 
+    },
+  },
+];
+
+const adminLayout = [
   {
-    "Google Chrome": window => {
+    "": (window) => {
+      // Screen 0 is always the primary window
+      moveToScreen(0)(window);
+    },
+  },
+  {
+    "Google Chrome": (window) => {
       moveToScreen(middleMonitor)(window);
     },
-    Jira: window => {
+    Code: (window) => {
+      moveToScreen(middleMonitor)(window);
+      left(window);
+    },
+    Hyper: (window) => {
+      moveToScreen(middleMonitor)(window);
+      right(window);
+    },
+    "": (window) => {
+      moveToScreen(offMonitor)(window);
+    },
+  },
+  {
+    "Google Chrome": (window) => {
+      moveToScreen(middleMonitor)(window);
+    },
+    Jira: (window) => {
       moveToScreen(tallMonitor)(window);
       up(window);
     },
-    Trello: window => {
+    Trello: (window) => {
       moveToScreen(tallMonitor)(window);
       up(window);
     },
-    Mail: window => {
+    Mail: (window) => {
       moveToScreen(tallMonitor)(window);
       down(window);
     },
-    "": window => {
+    "": (window) => {
       moveToScreen(offMonitor)(window);
-    }
-  }];
+    },
+  },
+];
 
 const applyLayout = (layoutScheme) => {
   const screens = Screen.all();
-  const layout = layoutScheme[screens.length - 1]
-  
+  const layout = layoutScheme[screens.length - 1];
+
   Window.all()
-    .filter(window => window.isVisible())
-    .forEach(window => {
+    .filter((window) => window.isVisible())
+    .forEach((window) => {
       const title = window.app().name();
-      const applicableRule = Object.keys(layout).find(rule => new RegExp(rule).test(title));
+      const applicableRule = Object.keys(layout).find((rule) =>
+        new RegExp(rule).test(title)
+      );
       layout[applicableRule](window);
     });
 };
 
-let currentLayout = "none"
+let currentLayout = "none";
 const toggleLayout = () => {
-  const layouts = ["dev", "admin"]
-  const currentIndex = layouts.indexOf(currentLayout)
-  const nextIndex = (currentIndex + 1) % layouts.length
+  const layouts = ["dev", "admin"];
+  const currentIndex = layouts.indexOf(currentLayout);
+  const nextIndex = (currentIndex + 1) % layouts.length;
 
-  currentLayout = layouts[nextIndex]
+  currentLayout = layouts[nextIndex];
 
-  Phoenix.log(JSON.stringify(currentLayout, null, 2))
+  Phoenix.log(JSON.stringify(currentLayout, null, 2));
 
   switch (currentLayout) {
     case "admin":
       applyLayout(adminLayout);
       break;
-    case "dev": 
+    case "dev":
     default:
       applyLayout(devLayout);
       break;
   }
-}
+};
 
-const moveToScreen = index => window => {
+const moveToScreen = (index) => (window) => {
   const screen = Screen.all()[index];
 
   if (screen) {
@@ -272,35 +272,35 @@ const moveToScreen = index => window => {
       x: bounds.x + gutter,
       y: bounds.y + gutter,
       width: bounds.width - 2 * gutter,
-      height: bounds.height - 2 * gutter
+      height: bounds.height - 2 * gutter,
     });
 
     // Hack to make sure the window is on the right monitor before resizing as otherwise the
     // window size will be constrained to the old screen's size.
-    setTimeout(moveToScreen.bind(null, index, window), 50)
+    setTimeout(moveToScreen.bind(null, index, window), 50);
   }
 };
 
-const moveToRelativeSpace = direction => () => {
+const moveToRelativeSpace = (direction) => () => {
   const window = Window.focused();
   const currentSpace = window.screen().currentSpace();
-  const nextSpace = direction >= 0 ? currentSpace.next() : currentSpace.previous();
+  const nextSpace =
+    direction >= 0 ? currentSpace.next() : currentSpace.previous();
   if (currentSpace && nextSpace) {
     currentSpace.removeWindows([window]);
     nextSpace.addWindows([window]);
   }
 };
 
-const focusApp = name => () => {
-  const app = App.get(name)
+const focusApp = (name) => () => {
+  const app = App.get(name);
 
   if (app) {
-    app.focus()
+    app.focus();
   }
-}
+};
 
 /* Keybindings */
-
 
 Key.on("space", ["shift", "alt", "ctrl"], /*fullsizeAllWindows*/ toggleLayout);
 
@@ -318,8 +318,12 @@ Key.on("w", ["shift", "alt", "ctrl"], topright);
 Key.on("a", ["shift", "alt", "ctrl"], bottomleft);
 Key.on("s", ["shift", "alt", "ctrl"], bottomright);
 
-Key.on("q", ["shift", "alt"], () => moveToScreen(tallMonitor)(Window.focused()));
-Key.on("w", ["shift", "alt"], () => moveToScreen(middleMonitor)(Window.focused()));
+Key.on("q", ["shift", "alt"], () =>
+  moveToScreen(tallMonitor)(Window.focused())
+);
+Key.on("w", ["shift", "alt"], () =>
+  moveToScreen(middleMonitor)(Window.focused())
+);
 Key.on("e", ["shift", "alt"], () => moveToScreen(offMonitor)(Window.focused()));
 
 Key.on("h", ["shift", "alt", "ctrl"], moveToRelativeSpace(-1));
